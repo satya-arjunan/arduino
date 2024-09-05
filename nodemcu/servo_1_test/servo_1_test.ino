7f2a2bc15607dd2b203a1c0eb4809451da647bc1
Satya Arjunan <satya.arjunan@gmail.com>
2024-09-05 21:58:37 +1000

    added nodeMCU servo test

---
+++ nodemcu/servo_1_test/servo_1_test.ino
@@ -0,0 +1,25 @@
+#include<Servo.h>
+
+Servo servo;
+int pos = 0;    // variable to store the servo position
+
+
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(5, 500, 2400);//D1 of NodeMcu
+  servo.write(0);
+  delay(2000);
+}
+
+
+void loop() {
+ for (pos = 1; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    servo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 1; pos -= 1) { // goes from 180 degrees to 0 degrees
+    servo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
