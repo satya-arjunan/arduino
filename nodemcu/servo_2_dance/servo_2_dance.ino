cac8d89ed39150f9b871b5dc2c1b6abdb5b02f6a
Satya Arjunan <satya.arjunan@gmail.com>
2024-09-25 10:03:24 +1000

    dance

--- nodemcu/servo_2_dance/servo_2_dance.ino
+++ nodemcu/servo_2_dance/servo_2_dance.ino
@@ -5,24 +5,66 @@ Servo servo2;
 
 
 void setup() {
-  // put your setup code here, to run once:
-  servo1.attach(5, 500, 2400);//D1 of NodeMcu
+  servo1.attach(D1, 500, 2400);
   servo1.write(0);
-  delay(1000);
-  servo2.attach(0, 500, 2400);
+  servo2.attach(D3, 500, 2400);
   servo2.write(0);
-  delay(1000);
 }
 
-void move(Servo& servo, int start, int end, int inc, int d) {
- for (int pos = start; pos <= end; pos += inc) { 
-    servo.write(pos); 
-    delay(d);
+void move(Servo& servo, int start, int end, int inc) {
+ for (int i=start; i <= end; i += inc) { 
+    servo.write(i);
   }
 }
 
+void short_move(Servo& servo, int count) {
+  for (int i=1; i <= count; i++) {
+    move(servo, 10, 30, 10);
+    delay(122);
+    move(servo, 30, 10, 10);
+    delay(122);
+  }
+}
+
+void second_move(Servo& servo, int count) {
+  for (int j=1; j <= count; ++j) {
+    for (int i=1; i <= 8; i++) {
+      move(servo, 10, 50, 10);
+      delay(110);
+    }
+    move(servo, 10, 90, 10);
+    delay(120);
+    move(servo, 10, 90, 10);
+    delay(120);
+  }
+  servo.write(10); 
+}
+
+void long_move(Servo& servo) {
+    move(servo, 10, 80, 10);
+    delay(100);
+}
+
+void slow_move(Servo& servo, int count) {
+  for (int i=1; i <=count; ++i) {
+    servo.write(130);
+    delay(800);
+    servo.write(10);
+    delay(800);
+    servo.write(130);
+    delay(200);
+    servo.write(10);
+    delay(200);
+  }
+}
 
 void loop() {
-  move(servo2, 10, 30, 5, 0);
+  short_move(servo2, 8);
+  long_move(servo2);
+  short_move(servo2, 14);
+  long_move(servo2);
+  second_move(servo1, 8);
+  long_move(servo2);
   delay(300);
+  slow_move(servo1, 9);
 }
